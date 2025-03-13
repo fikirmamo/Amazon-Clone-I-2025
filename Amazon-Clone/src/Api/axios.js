5798b8bcda5cd9d526d77c45c0301d4a8e2e1aa0
Fikirte M Offiga <fikirmamo@gmail.com>
2025-03-13 13:52:14 -0400

    redeploying the modified version

--- Amazon-Clone/src/Api/axios.js
+++ Amazon-Clone/src/Api/axios.js
@@ -6,9 +6,11 @@ const axiosInstance = axios.create({
   //deployed version of firebase function
   //"#"
   //deployed version of amazon server on render.com
-  // baseURL: "https://amazon-api-deploy-k3bn.onrender.com/",
+  baseURL: "https://amazon-api-deploy-2-srbi.onrender.com/",
+
+  //local
   // baseURL: "http://localhost:5000/",
-  baseURL: "http://127.0.0.1:5001/clone-373f5/us-central1/api",
+  
 });
 
 export {axiosInstance};
